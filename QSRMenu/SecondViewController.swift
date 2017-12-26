8df30970e6c374ea3f5993830719804ff36ccd38
Kevin Munc <muncman@gmail.com>
2017-12-26 14:31:37 -0500

    Initial Commit

---
+++ QSRMenu/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  QSRMenu
+//
+//  Created by Kevin Munc on 12/26/17.
+//  Copyright © 2017 Method Up. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
